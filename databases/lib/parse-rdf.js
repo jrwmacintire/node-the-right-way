84514be99c11f46b6faf5761f295bffb47a1aa5b
John MacIntire <jrwmacintire@gmail.com>
2019-07-19 14:26:10 -0700

    Adding database tutorial directory.

---
+++ databases/lib/parse-rdf.js
@@ -0,0 +1,7 @@
+'use strict';
+const cheerio = require('cheerio');
+
+module.exports = rdf => {
+    const book = {};
+    return book;
+};
---
+++ databases/test/parse-rdf-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fs = require('fs');
+const expect = require('chai').expect;
+const parseRDF = require('../lib/parse-rdf.js');
+
+const rdf = fs.readFileSync(`${__dirname}/pg16.rdf`);
+
+describe('parseRDF', () => {
+    it('should be a function', () => {
+        expect(parseRDF).to.be.a('function');
+    });
+
+    it('should parse RDF content', () => {
+        const book = parseRDF(rdf);
+        expect(book).to.be.an('object');
+        expect(book).to.have.a.property('id', 16);
+    });
+})
