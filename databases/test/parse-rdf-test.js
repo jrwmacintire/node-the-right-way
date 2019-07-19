3e7acddbd5990db0ccddb867516e722c80d9aac1
John MacIntire <jrwmacintire@gmail.com>
2019-07-19 15:34:45 -0700

    Added two BDD tests for RDF conversion with Cheerio

--- databases/test/parse-rdf-test.js
+++ databases/test/parse-rdf-test.js
@@ -15,5 +15,16 @@ describe('parseRDF', () => {
         const book = parseRDF(rdf);
         expect(book).to.be.an('object');
         expect(book).to.have.a.property('id', 16);
+        expect(book).to.have.a.property('title', 'Peter Pan');
+        expect(book).to.have.a.property('authors')
+            .that.is.an('array').with.lengthOf(1)
+            .and.contains('Barrie, J. M. (James Matthew)');
+        expect(book).to.have.a.property('subjects')
+            .that.is.an('array').with.lengthOf(5)
+            .and.contains('Fantasy literature')
+            .and.contains('Pirates -- Fiction')
+            .and.contains('Peter Pan (Fictitious character) -- Fiction')
+            .and.contains('Never-Never Land (Imaginary place) -- Fiction')
+            .and.contains('Fairies -- Fiction');
     });
 })
